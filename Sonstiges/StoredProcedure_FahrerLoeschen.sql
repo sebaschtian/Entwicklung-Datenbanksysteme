1d0b68fa8475c1618d6d565434bde5ad184b9034
sebaschtian <sebastian.rieg@gmx.de>
2026-05-19 09:35:20 +0200

    Verbesserung, FahrerLoeschen

---
+++ Sonstiges/StoredProcedure_FahrerLoeschen.sql
@@ -0,0 +1,29 @@
+DELIMITER //
+
+CREATE PROCEDURE FahrerLoeschen(
+    IN p_FahrerID INT,
+    IN p_TeamName VARCHAR(255)
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    START TRANSACTION;
+        DELETE FROM nimmtTeil
+        WHERE FahrerID = p_FahrerID AND TeamName = p_TeamName;
+
+        DELETE FROM Training
+        WHERE FahrerID = p_FahrerID AND TeamName = p_TeamName;
+
+        DELETE FROM Telefonnummer
+        WHERE FahrerID = p_FahrerID AND TeamName = p_TeamName;
+
+        DELETE FROM Fahrer
+        WHERE FahrerID = p_FahrerID AND TeamName = p_TeamName;
+    COMMIT;
+END //
+
+DELIMITER ;
